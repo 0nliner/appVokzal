79e3c5e0c2b5e2192a18d0091c48d86188651d37
Vladislav <timba20@yandex.ru>
2021-07-06 16:10:26 +0300

    trips page

--- src/pages/Trips/index.js
+++ src/pages/Trips/index.js
@@ -1,115 +1,145 @@
-import {Box, Card, Container, Grid, Typography} from "@material-ui/core";
+import {Box, Card, Container, Grid, Typography, makeStyles, CardActionArea} from "@material-ui/core";
 import {Header} from "../components/Header";
+import React from "react";
+
+import CardContent from '@material-ui/core/CardContent';
 // icons
 import ArrowSVG from "../../icons/arrow.svg";
 import BasketSVG from "../../icons/basket.svg";
 import PenSVG from "../../icons/Pen.svg";
+import TrendingFlatIcon from '@material-ui/icons/TrendingFlat';
+import CreateIcon from '@material-ui/icons/Create';
+
+const data = [
+  [
+    {
+      city1: "Казань",
+      city2: "Оренбург",
+      date: "10.08.2021",
+      time: "10:30",
+      seats: "3",
+    }
+  ],
+  [
+    {
+      city1: "Сочи",
+      city2: "Москва",
+      date: "11.08.2021",
+      time: "09:40",
+      seats: "2",
+    },
+    {
+      city1: "Москва",
+      city2: "Сочи",
+      date: "13.08.2021",
+      time: "08:00",
+      seats: "2",
+    }
+  ],
+  
+]
 
 
+const useTripStyles = makeStyles(theme => ({
+  root: {
+    marginTop: 20
+  },
+  content: {
+    width: "100%"
+  },
+  pen: {
+    display: "flex",
+    justifyContent: "flex-end",
+    width: "90%",
+  },
+  city: {
+    width: "90%",
+    display: "flex",
+  },
+  info: {
+    width: "90%",
+    display: "flex",
+    justifyContent: "flex-start"
+  }
+}))
 
 function Trip (props) {
+    const classes = useTripStyles();
+
     return (
-        <Grid item style={{marginTop: 20, position: "relative"}}>
-            <div style={{position: "absolute", right: 5, top: 5}}>
-                <img src={PenSVG}/>
-            </div>
-            <Card>
-                <Container>
-                    <Grid container direction={"column"}>
-                        {/*  верхняя строка */}
-                        <Grid item xs={"12"} style={{marginTop: 10}}>
-                            <Typography variant={"h6"} align={"left"}>
-                                Сочи
-                            </Typography>
-                        </Grid>
-
-                        <Grid item xs={"12"} align={"left"}>
-                            <img src={ArrowSVG} style={{transform: "rotate(90deg)"}}/>
-                        </Grid>
-
-                        <Grid item>
-                            <Typography variant={"h6"} align={"left"}>
-                                Сантк-Петербург
-                            </Typography>
-                        </Grid>
-
-                        <Typography align={"left"}>
-                            дата:
-                        </Typography>
-
-                        <Typography align={"left"}>
-                            время:
-                        </Typography>
-
-                        <Typography align={"left"}>
-                            места:
-                        </Typography>
-
-                        {/* обратная дорога*/}
-                        <Typography variant={"h5"} align={"left"} style={{color: "#91B3FA", marginTop: 20}}>
-                            обратно
-                        </Typography>
-
-                        <Grid item xs={"12"} style={{marginTop: 10}}>
-                            <Typography variant={"h6"} align={"left"}>
-                                Сантк-Петербург
-                            </Typography>
-                        </Grid>
-
-                        <Grid item xs={"12"} align={"left"}>
-                            <img src={ArrowSVG} style={{transform: "rotate(90deg)"}}/>
-                        </Grid>
-
-                        <Grid item>
-                            <Typography variant={"h6"} align={"left"}>
-                                Сочи
-                            </Typography>
-                        </Grid>
-
-                        <Typography align={"left"}>
-                            дата:
-                        </Typography>
-
-                        <Typography align={"left"}>
-                            время:
-                        </Typography>
-
-                        <Typography align={"left"}>
-                            места:
-                        </Typography>
-
-
-
-                    </Grid>
-                </Container>
+      <Grid container xs={"md"} direction={"column"}>
+        {data.map(item =>{
+          return (
+            <Card className={classes.root}>
+               <CardActionArea>
+                 {item.map(trip => {
+            return (
+                
+                  <CardContent className={classes.content}>
+                    {item.indexOf(trip) === 0 && 
+                    <div className={classes.pen}><CreateIcon style={{fontSize: "20px"}} /></div>
+                    }
+                  <div className={classes.city}>
+                    <Typography style={{marginTop: 5}} align="left" variant="h6">{trip.city1}</Typography>
+                    <TrendingFlatIcon style={{fontSize: "40px"}} />
+                    <Typography style={{marginTop: 5}}  align="left" variant="h6">{trip.city2}</Typography>
+                  </div>
+                  <div className={classes.info}>
+                   <Typography style={{width: 47}} align="left" variant="body2">Дата:</Typography>
+                   <Typography style={{marginLeft: "10%"}} align="left" variant="body2">{trip.date}</Typography>
+                  </div>
+                  <div className={classes.info}>
+                    <Typography style={{width: 47}}  align="left" variant="body2">Время:</Typography>
+                    <Typography  style={{marginLeft: "10%"}} align="left" variant="body2">{trip.time}</Typography>
+                  </div>
+                  <div className={classes.info}>
+                    <Typography style={{width: 47}}  align="left" variant="body2">Места:</Typography>
+                    <Typography  style={{marginLeft: "10%"}} align="left" variant="body2">{trip.seats}</Typography>
+                  </div>
+                  </CardContent>
+            )
+          })}
+               </CardActionArea>
             </Card>
-        </Grid>
+          )
+          
+          
+        })}
+      </Grid>
+      
+        // <Card>
+        //   <CardActionArea>
+        //     <CardContent className={classes.content}>
+        //       <div className={classes.pen}><CreateIcon style={{fontSize: "20px"}} /></div>
+        //       <div className={classes.city}>
+        //         <Typography style={{marginTop: 5}} align="left" variant="h6">Сочи</Typography>
+        //         <TrendingFlatIcon style={{fontSize: "40px"}} />
+        //         <Typography style={{marginTop: 5}}  align="left" variant="h6">Санкт-Петербург</Typography>
+        //       </div>
+        //       <div className={classes.city}>
+        //         <Typography  align="left" variant="body2">Дата:</Typography>
+        //         <Typography style={{width: 65}} align="left" variant="caption">10.08.2021</Typography>
+        //       </div>
+              // <div className={classes.city}>
+              //   <Typography  align="left" variant="body2">Время:</Typography>
+              //   <Typography  style={{width: 65}} align="left" variant="caption">10:30</Typography>
+              // </div>
+              // <div className={classes.city}>
+              //   <Typography  align="left" variant="body2">Места:</Typography>
+              //   <Typography  style={{width: 65}} align="left" variant="caption">3</Typography>
+              // </div>
+        //     </CardContent>
+        //   </CardActionArea>
+        // </Card>
     );
 }
 
 function Trips (props) {
-    let trips = [
-        {
-            id: "0",
-            from: "name",
-            to: "name",
-            price: "1000",
-            date: "10.08.2021",
-            places: "4",
 
-            backId: "1",
-            backFrom: "name",
-            backTo: "name",
-            backPrice: "1000",
-            backDate: "10.08.2021",
-            backPlaces: "4"
-        },
-        2
-    ];
     return (
         <Container>
             <Grid container xs={"md"} direction={"column"}>
-                {trips.map(trip => <Trip {...trip}/>)}
+                <Trip></Trip>
             </Grid>
         </Container>
     );
