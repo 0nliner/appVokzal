8682c69baf7037ec715c76b2a5eb70e9ce59a48c
Vladislav <timba20@yandex.ru>
2021-07-25 22:40:19 +0300

    -- edit navigation

--- src/pages/components/Nav.js
+++ src/pages/components/Nav.js
@@ -15,11 +15,25 @@ import { Avatar, Typography } from '@material-ui/core';
 import FlagIcon from '@material-ui/icons/Flag';
 import Link from '@material-ui/core/Link'
 
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+
+import avatarPhoto from '../../images/avatar.png'
+import { ReactComponent as CodeIcon } from '../../icons/code.svg';
+import { ReactComponent as GiftIcon} from '../../icons/gift.svg';
+import { ReactComponent as HistoryIcon} from '../../icons/history.svg';
+import { ReactComponent as HelpIcon} from '../../icons/help.svg';
+import { ReactComponent as CardIcon} from '../../icons/card2.svg';
+import { ReactComponent as SettingIcon} from '../../icons/setting.svg';
+
+
 
 const useStyles = makeStyles((theme)=>({
         root: {
             display: 'flex',
             justifyContent: 'center',
+            paddingBottom: 10
         },
         list: {
             width: 250,
@@ -32,11 +46,14 @@ const useStyles = makeStyles((theme)=>({
             color: "rgba(62, 89, 130, 1)"
         },
         large: {
-            width: theme.spacing(7),
-            height: theme.spacing(7),
+            width: theme.spacing(14),
+            height: theme.spacing(14),
+            filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
         }
     })
 );
+
+
 
 export function Nav() {
     const classes = useStyles();
@@ -47,6 +64,7 @@ export function Nav() {
         right: false,
     });
 
+
     const toggleDrawer = (anchor, open) => (event) => {
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
             return;
@@ -54,6 +72,47 @@ export function Nav() {
 
         setState({ ...state, [anchor]: open });
     };
+
+
+    const [stateS, setStateSwitch] = React.useState({
+        checked: false,
+      });
+    
+      const handleChangeSwitch = (event) => {
+        setStateSwitch({ ...stateS, [event.target.name]: event.target.checked });
+      };
+      const data = [
+        {
+            index: 0,
+            name: "Код",
+            icon: <CodeIcon style={{fontSize:"40px", paddingLeft: 5}} />
+        },
+        {
+            index: 1,
+            name: "Подарок бонусы",
+            icon: <GiftIcon style={{fontSize:"40px", paddingLeft: 5}} />
+        },
+        {
+            index: 2,
+            name: "История",
+            icon: <HistoryIcon style={{fontSize:"40px"}} />
+        },
+        {
+            index: 3,
+            name: "Помощь",
+            icon: <HelpIcon style={{fontSize:"40px", paddingLeft:10}} />
+        },
+        {
+            index: 4,
+            name: "Оплата",
+            icon: <CardIcon style={{fontSize:"40px", paddingLeft: 5}} />
+        },
+        {
+            index: 5,
+            name: "Настройки",
+            icon: <SettingIcon style={{fontSize:"40px", paddingLeft:5}} />
+        },
+    ]
 
     const list = (anchor) => {
         const link = (param) => {
@@ -70,34 +129,31 @@ export function Nav() {
                 return "/findTrip";
               case 5:
                 return "/observeTrip";
-              case 6:
-                return "/map";
-              case 7:
-                return "/dialog";
-              case 8:
-                return "/createTrip";
+              
               default:
                 return "/map";
             }
           }
+          
+
         return (
         <div
             className={clsx(classes.list, {
                 [classes.fullList]: anchor === 'top' || anchor === 'bottom',
             })}
             role="presentation"
-            onClick={toggleDrawer(anchor, false)}
+            
             onKeyDown={toggleDrawer(anchor, false)}
         >
 
             {/* TODO: как появятся иконки для навигации - подкючить их тут*/}
             <List>
                 <div className={classes.root}>
-                <Avatar alt="Avatar Image" src="../../images/avatar.png" className={classes.large} />
+                <Avatar alt="Avatar Image" src={avatarPhoto} className={classes.large} />
                 </div>
-                <Typography align="center" variant="h5">Elon Mask</Typography>
-                <Typography align="center">Timba20@yandex.ru</Typography>
-                <Typography align="center">+79320964396</Typography>
+                <Typography style={{color: "#757575", fontWeight: 600}}  align="center" variant="h6">Дмитрий Дмитриевич</Typography>
+                <Typography style={{color: "#757575"}} align="center">test@gmail.com</Typography>
+                <Typography style={{color: "#757575"}} align="center">+7 (987) 226 98 56</Typography>
                 {/* {['Код', 'подарок и бонусы', 'История', 'Помощь', 'Настройки'].map((text, index) => (*/}
                 {/*    <ListItem button key={'Код'}>*/}
                 {/*         это типо для иконок навигации*/}
@@ -106,17 +162,23 @@ export function Nav() {
                     {/*</ListItem>*/}
                 {/*))} */}
             </List>
-            <Divider />
-            <List>
+            <List style={{marginTop: 20}}>
                 {/* <ListItem>
                     <ListItemIcon> <FlagIcon /> </ListItemIcon>
                     <ListItemText primary="Код"></ListItemText>
                 </ListItem> */}
-                {['Профиль', 'Поездка', 'Все поездки', 'Бронирование', 'Найти поездку', 'Выбрать поездку', 'Карта', 'Чат', 'Создать поездку'].map((text, index) => (
-                    <ListItem button  component={Link} href={link(index)}>
-                        {/*<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>*/}
-                        <ListItemText primary={text} />
+                {data.map(item => (
+                    <ListItem button component={Link} href={link(item.index)}>
+                        <ListItemIcon style={{color: "black"}}>{item.icon}</ListItemIcon>
+                        <ListItemText style={{color: "#757575"}} primary={item.name} />
                     </ListItem>))}
+                    
+                    <ListItem>
+                        <ListItemIcon style={{width: 56}}>
+                        <FormControlLabel control={ <Switch checked={stateS.checked} onChange={handleChangeSwitch} name="checked" color="primary"/>} />
+                        </ListItemIcon>
+                        <ListItemText style={{color: "#757575"}} primary="Водитель" />
+                    </ListItem>
 
 
                 {/* todo: внизу нормальная менбшка + иконки к этому добру, раскомментить
